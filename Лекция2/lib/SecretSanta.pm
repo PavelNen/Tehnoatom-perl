512df88acb803cd749327394bf6d6ed6d3bb5d55
PavelNen <nps-11@mail.ru>
2017-03-05 00:11:23 +0300

    Почти работает

--- Лекция2/lib/SecretSanta.pm
+++ Лекция2/lib/SecretSanta.pm
@@ -9,8 +9,64 @@ sub calculate {
 	my @members = @_;
 	my @res;
 	# ...
-	#	push @res,[ "fromname", "toname" ];
+	#	push @res,[ "from_name", "to_name" ];
 	# ...
+	
+	#Рассчёт итогового количества пар, оно равно количеству всех участников
+	my $k = 0;
+#	p @members;
+	for my $mem (@members) {
+	    ref $mem eq 'ARRAY' ? $k += 2: $k++;
+	}
+	#конец----------------
+	
+	
+	my %hash = ();	#Хэш с парами дарителей и принимающих подарки
+	my $size = $#members+1;  #Размер массива с участниками
+	
+	
+	my $from_supr; my $to_supr; #Номера супругов в паре
+	
+	my $from; #Номер случайного элемента масива @members в качестве дарителя
+	#Если в ячейке массива супруги, то нужно взять случайного супруга с номером 0 или 1, иначе -1
+#	if (ref \@members[$from] eq "ARRAY") {$from_supr = int(rand 1);} else {$from_supr = -1; }
+	my $to; #То же для принимающего подарок
+#	if (ref \@members[$from] eq "ARRAY") {$to_supr = int(rand 1);} else {$to_supr = -1;}
+	
+	#Начинаем составлять список (хэш) Даритель => Принимающий
+	my $from_name; my $to_name;   #Имена пар
+	for(1..$k){
+	    $from = int(rand $size);
+	    if (ref $members[$from] eq 'ARRAY') { $from_supr = int(rand 2)} else {$from_supr = -1;}
+	    $to = int(rand $size);
+	    if (ref $members[$to] eq 'ARRAY') {$to_supr = int(rand 2)} else {$to_supr = -1;}
+	    
+	    if ($from != $to){
+	    
+		#Если нет ещё такого дарителя, либо если он не получал от другого подарка, которому собирается подарить,
+		#то записываем в хэш, иначе подбираем заново
+		
+		
+		if ($from_supr == -1) {$from_name = $members[$from];} else {$from_name = $members[$from][$from_supr];}
+		if ($to_supr == -1) {$to_name = $members[$to]} else {$to_name = $members[$to][$to_supr];}
+		
+		if (!exists $hash{$from_name}
+		    ||
+		    exists $hash{$to_name}
+		    &&
+		    $hash{$to_name} eq $from_name)
+		{
+		    $hash{$from_name} = $to_name;
+		    
+		}
+	
+	    }
+	    else {redo;}
+	    
+	}
+	
+	for my $key (keys %hash) {push @res,[ $key, $hash{$key} ];}
+	
 	return @res;
 }
 
