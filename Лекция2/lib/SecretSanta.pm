a9141deb56407dd838bbf1bbdd4553a1190a3996
PavelNen <nps-11@mail.ru>
2017-03-05 05:09:28 +0300

    Работает

--- Лекция2/lib/SecretSanta.pm
+++ Лекция2/lib/SecretSanta.pm
@@ -20,7 +20,7 @@ sub calculate {
 	}
 	#конец----------------
 	
-	
+
 	my %hash = ();	#Хэш с парами дарителей и принимающих подарки
 	my $size = $#members+1;  #Размер массива с участниками
 	
@@ -32,15 +32,18 @@ sub calculate {
 #	if (ref \@members[$from] eq "ARRAY") {$from_supr = int(rand 1);} else {$from_supr = -1; }
 	my $to; #То же для принимающего подарок
 #	if (ref \@members[$from] eq "ARRAY") {$to_supr = int(rand 1);} else {$to_supr = -1;}
-	
+#	say $k;
 	#Начинаем составлять список (хэш) Даритель => Принимающий
 	my $from_name; my $to_name;   #Имена пар
-	for(1..$k){
+	
+	my $i = 0;
+	
+	while($i<$k){
 	    $from = int(rand $size);
 	    if (ref $members[$from] eq 'ARRAY') { $from_supr = int(rand 2)} else {$from_supr = -1;}
 	    $to = int(rand $size);
 	    if (ref $members[$to] eq 'ARRAY') {$to_supr = int(rand 2)} else {$to_supr = -1;}
-	    
+	   # print "$i ";
 	    if ($from != $to){
 	    
 		#Если нет ещё такого дарителя, либо если он не получал от другого подарка, которому собирается подарить,
@@ -50,13 +53,14 @@ sub calculate {
 		if ($from_supr == -1) {$from_name = $members[$from];} else {$from_name = $members[$from][$from_supr];}
 		if ($to_supr == -1) {$to_name = $members[$to]} else {$to_name = $members[$to][$to_supr];}
 		
-		if (!exists $hash{$from_name}
-		    ||
-		    exists $hash{$to_name}
-		    &&
-		    $hash{$to_name} eq $from_name)
+		if (!(exists $hash{$from_name}))
 		{
-		    $hash{$from_name} = $to_name;
+		    my $f = 0;
+		    for my $value (values %hash) {$f = 1 if $value eq $to_name && $hash{$to_name} eq $from_name}
+		    if (!$f) {
+			$hash{$from_name} = $to_name;
+			$i++; #Счётчик прибавляется только после успешного образования пары
+		    }
 		    
 		}
 	
@@ -66,7 +70,7 @@ sub calculate {
 	}
 	
 	for my $key (keys %hash) {push @res,[ $key, $hash{$key} ];}
-	
+
 	return @res;
 }
 
