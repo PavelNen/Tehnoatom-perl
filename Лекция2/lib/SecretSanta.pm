d2c05c7440b4a3dc3e8c9425b83e3c403c6a32d9
PavelNen <nps-11@mail.ru>
2017-03-06 00:21:47 +0300

    фасочка

--- Лекция2/lib/SecretSanta.pm
+++ Лекция2/lib/SecretSanta.pm
@@ -28,45 +28,90 @@ sub calculate {
 	my $from_supr; my $to_supr; #Номера супругов в паре
 	
 	my $from; #Номер случайного элемента масива @members в качестве дарителя
-	#Если в ячейке массива супруги, то нужно взять случайного супруга с номером 0 или 1, иначе -1
-#	if (ref \@members[$from] eq "ARRAY") {$from_supr = int(rand 1);} else {$from_supr = -1; }
-	my $to; #То же для принимающего подарок
-#	if (ref \@members[$from] eq "ARRAY") {$to_supr = int(rand 1);} else {$to_supr = -1;}
-#	say $k;
+		  #Если в ячейке массива супруги, то нужно взять случайного супруга с номером 0 или 1, иначе -1
+	my $to;   #То же для принимающего подарок
+	
 	#Начинаем составлять список (хэш) Даритель => Принимающий
 	my $from_name; my $to_name;   #Имена пар
 	
 	my $i = 0;
 	
-	while($i<$k){
-	    $from = int(rand $size);
-	    if (ref $members[$from] eq 'ARRAY') { $from_supr = int(rand 2)} else {$from_supr = -1;}
-	    $to = int(rand $size);
-	    if (ref $members[$to] eq 'ARRAY') {$to_supr = int(rand 2)} else {$to_supr = -1;}
-	   # print "$i ";
-	    if ($from != $to){
-	    
-		#Если нет ещё такого дарителя, либо если он не получал от другого подарка, которому собирается подарить,
-		#то записываем в хэш, иначе подбираем заново
-		
-		
-		if ($from_supr == -1) {$from_name = $members[$from];} else {$from_name = $members[$from][$from_supr];}
-		if ($to_supr == -1) {$to_name = $members[$to]} else {$to_name = $members[$to][$to_supr];}
-		
-		if (!(exists $hash{$from_name}))
-		{
-		    my $f = 0;
-		    for my $value (values %hash) {$f = 1 if $value eq $to_name && $hash{$to_name} eq $from_name}
-		    if (!$f) {
-			$hash{$from_name} = $to_name;
-			$i++; #Счётчик прибавляется только после успешного образования пары
-		    }
-		    
-		}
 	
-	    }
-	    else {redo;}
+	while($i<$k){
+		START:
+		$from = int(rand $size);
+		if (ref $members[$from] eq 'ARRAY') { $from_supr = int(rand 2)} else {$from_supr = -1;}
+		$to = int(rand $size);
+		if (ref $members[$to] eq 'ARRAY') {$to_supr = int(rand 2)} else {$to_supr = -1;}
 	    
+		my $f1 = 0; # Флаг наличия дарителя в хэше в своей роли, а принимателя в своей
+		my $f2 = 0; # Флаг наличия дарителя в хэше в роли принимателя, а принимателя в другой
+
+		    
+		if ($from != $to){	#Проверяем претендентов из разных ячеек @members
+	    
+			#Проверяем, чтобы не было уже такой пары, прямой или обратной,
+			#и записываем в хэш, иначе подбираем заново
+		
+					
+			if ($from_supr == -1) {$from_name = $members[$from];} else {$from_name = $members[$from][$from_supr];}
+			if ($to_supr == -1) {$to_name = $members[$to]} else {$to_name = $members[$to][$to_supr];}
+		
+		    
+			if (exists $hash{$from_name}) {$f1 = 1;} #Если этот даритель уже дарил
+			for my $value (values %hash) {if ($value eq $to_name) {$f1 = 1;}} #Если этот получатель уже получал
+		    		    
+			if (exists $hash{$to_name}) {$f2 = 1;} # Если этот получатель уже дарил
+			for my $value (values %hash) {if ($value eq $from_name) {$f2 = 1;}} #Если этот даритель уже получал
+			
+		    
+			if (!$f1 && ($i<$k-2 || $f2)) {
+				$hash{$from_name} = $to_name;
+				$i++; #Счётчик прибавляется только после успешного образования пары
+			}
+			    elsif (!$f1 && !$f2 && $i == $k-2) {$i = 0; %hash = (); goto START;}    #Когда остаётся заполнить одну или две пары,
+				elsif (!$f1 && !$f2 && $i == $k-1) {$i = 0; %hash = (); goto START;}#появляются специальные ограничения для претендентов
+		
+	
+		}
+		    elsif ($i == $k-1 && $from == $to) { # Обрабатывем случаи, когда претенденты оказались из одной ячейки
+		    
+			   # say "three";
+			    if (ref $members[$from] eq ''){
+				    if (exists $hash{$from}) {$f1 = 1;}
+				    for my $value (values %hash) {if ($value eq $to) {$f1 = 1;}}
+				    
+				    if (exists $hash{$from}) {$f2 = 1;}
+				    for my $value (values %hash) {if ($value eq $to) {$f2 = 1;}}
+				    if (!$f1) {$i = 0; %hash = (); goto START;}
+			    }
+				else{
+					$from_name = $members[$from][0];
+					$to_name = $members[$from][1];
+					
+					if (exists $hash{$from_name}) {$f1 = 1;}
+					for my $value (values %hash) {if ($value eq $to_name) {$f1 = 1;}}
+		
+					if (exists $hash{$to_name}) {$f2 = 1;}
+					for my $value (values %hash) {if ($value eq $from_name) {$f2 = 1;}}
+					
+					if (!$f1 && !$f2) {$i = 0; %hash = (); goto START;}
+				}
+		    }
+			elsif ($i == $k-2  && $from == $to && ref $members[$from] eq 'ARRAY') {
+				
+				$from_name = $members[$from][0];
+				$to_name = $members[$to][1];
+		
+				if (exists $hash{$from_name}) {$f1 = 1;}
+				for my $value (values %hash) {if ($value eq $to_name) {$f1 = 1;}}
+		    		   
+				if (exists $hash{$to_name}) {$f2 = 1;}
+				for my $value (values %hash) {if ($value eq $from_name) {$f2 = 1;}}
+		  
+				if (!$f1 && !$f2){$i = 0; %hash = (); goto START;}
+			}
+		
 	}
 	
 	for my $key (keys %hash) {push @res,[ $key, $hash{$key} ];}
