0f0283bbaf8b74e1833b1e0cf1ee3d009cf2bd84
PavelNen <nps-11@mail.ru>
2017-03-16 12:34:06 +0300

    Задача заварилась

--- Lecture4/log-analyzer/bin/analyze.pl
+++ Lecture4/log-analyzer/bin/analyze.pl
@@ -12,6 +12,10 @@ my $parsed_data = parse_file($filepath);
 report($parsed_data);
 exit;
 
+
+our $IP
+
+
 sub parse_file {
     my $file = shift;
 
@@ -19,12 +23,16 @@ sub parse_file {
 
     my $result;
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
+
     while (my $log_line = <$fd>) {
 
         # you can put your code here
         # $log_line contains line from log file
+	($IP_adress, $day, $mon, $year, $hh, $mm, $ss, $tzone, $req, $scode, $nofbytes, $refferer, $)
+	$log_line =~ /^(\d+\.\d+\.\d+\.\d+) \[(\d{2})\/(\w{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4})\] "([^"]+)" (\d{3}) (\d+) "([^"]+)" "([^"]+)" "([^"]+)"/;
 
     }
+
     close $fd;
 
     # you can put your code here
