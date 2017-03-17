f8ef0c247c01fccd8637808662f725d0f2dcaf8f
PavelNen <nps-11@mail.ru>
2017-03-17 03:00:43 +0300

    Сделано

--- Lecture4/log-analyzer/bin/analyze.pl
+++ Lecture4/log-analyzer/bin/analyze.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl
+use 5.010;
 
 use strict;
 use warnings;
+use Time::Local;
+use diagnostics;
+use DDP;
+use Switch;
 
 
 my $filepath = $ARGV[0];
@@ -12,37 +17,111 @@ my $parsed_data = parse_file($filepath);
 report($parsed_data);
 exit;
 
+# Функция замены названия месяца на его номер
+sub mtod {
+  switch (shift){
+    case 'Jan' {return 1;}
+    case 'Feb' {return 2;}
+    case 'Mar' {return 3;}
+    case 'Apr' {return 4;}
+    case 'May' {return 5;}
+    case 'Jun' {return 6;}
+    case 'Jul' {return 7;}
+    case 'Aug' {return 8;}
+    case 'Sep' {return 9;}
+    case 'Oct' {return 10;}
+    case 'Nov' {return 11;}
+    case 'Dec' {return 12;}
+  }
+}
 
-our $IP
-
+=head1 parse_file(file)
+  Функция записывает результат парса в хеш %{$result}.
+  Ключом является total или IP адресс. Значениями - названия столбцов будущей таблицы.
+  Но под ключём total есть одно отличие. Под ключом $data_code есть ещё хеш,
+  в котором ключи это коды ошибок, а значения - килобайты. Это нужно для сортировке по кодам при выводе.
+=cut
 
 sub parse_file {
     my $file = shift;
 
-    # you can put your code here
+    my $result; # Ссылка на хеш
+    my $time; my $timemin; my $timemax;
+    my ($IP, $day, $mon, $mo, $year, $hh, $mm, $ss, $tzone, $req, $code, $nofbytes,$refferer, $uagent);
+    my $cratio = 1;
 
-    my $result;
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
 
+
     while (my $log_line = <$fd>) {
+      # Парсим строку
+      ($IP,
+		    $day, $mon, $year,
+		      $hh, $mm, $ss, $tzone,
+		        $req, $code, $nofbytes,
+		          $refferer, $uagent, $cratio) =
+		  $log_line =~ /^(\d+\.\d+\.\d+\.\d+) \[(\d{2})\/(\w{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4})\] "(.+)" (\d{3}) (\d+) "(.+)" "(.+)" "(.+)"$/;
+      if ($cratio eq '-') {$cratio = 1;}
 
-        # you can put your code here
-        # $log_line contains line from log file
-	($IP_adress, $day, $mon, $year, $hh, $mm, $ss, $tzone, $req, $scode, $nofbytes, $refferer, $)
-	$log_line =~ /^(\d+\.\d+\.\d+\.\d+) \[(\d{2})\/(\w{3})\/(\d{4}):(\d{2}):(\d{2}):(\d{2}) (\+\d{4})\] "([^"]+)" (\d{3}) (\d+) "([^"]+)" "([^"]+)" "([^"]+)"/;
+      $mo = mtod($mon);
+      $time = timelocal($ss, $mm, $hh, $day, $mo, $year);
 
+      $result->{'total'}{'count'} += 1;
+    #  p $result->{'total'}{'count'};
+      $result->{$IP}{'count'} += 1;
+
+      if ($result->{'total'}{'count'} == 1 || $timemax == $timemin) {
+        $timemin = $time; $timemax = $time;
+        $result->{'total'}{'avg'} = 0;
+        $result->{$IP}{'avg'} = 0;
+      }
+      else {
+        if ($time < $timemin) {$timemin = $time;}
+        if ($time > $timemax) {$timemax = $time;}
+        $result->{'total'}{'avg'} = 60 * $result->{'total'}{'count'} / ($timemax - $timemin);
+        $result->{$IP}{'avg'} = 60 * $result->{$IP}{'count'} / ($timemax - $timemin);
+      }
+
+      if ($code == 200) {
+        $result->{'total'}{'data'} += $nofbytes * $cratio / 1024;
+        $result->{$IP}{'data'} += $nofbytes * $cratio / 1024;
+      }
+      else {
+        $result->{'total'}{'data_code'}{$code} += $nofbytes / 1024;
+        $result->{$IP}{$code} += $nofbytes / 1024;
+      }
     }
 
     close $fd;
 
-    # you can put your code here
+    # you can put your advt here
 
     return $result;
 }
 
 sub report {
     my $result = shift;
+    #say join "\t", keys %{$result->{'total'}} ;
+    say "IP\tcount\tavg\tdata\tdata_" . join "\tdata_",
+      sort keys %{$result->{'total'}{'data_code'}};
+    my $i = 0;
 
-    # you can put your code here
+    for my $key (sort {$result->{$b}{'count'} <=>  $result->{$a}{'count'}} keys %{$result}) {
+      $i++;
+      #IP count avg data
+      print "$key\t" . $result->{$key}{'count'}; print "\t";
+      print $result->{$key}{'avg'} =~ /(\d+\.?\d?\d?)/; print "\t";
+      if (exists $result->{$key}{'data'}) {print $result->{$key}{'data'} =~ /(\d+\.?\d?\d?)/; print "\t";}
+        else {print "0\t";}
+      #data_xxx data_xxx data_xxx....
+      for my $data_key (sort keys %{$result->{'total'}{'data_code'}}){
+        if (exists $result->{$key}{$data_key}) {print $result->{$key}{$data_key} =~ /(\d+\.?\d?\d?)/; print "\t";}
+          else {print "0\t";}
+      }
+      print "\n";
+      exit if $i == 10;
+    }
+
+    # you can put your advt here
 
 }
