f21959733623872e179a1ea770cb7f4ae92b0cb8
PavelNen <nps-11@mail.ru>
2017-04-12 20:28:37 +0300

    Всё ближе и ближе..

--- Lecture8/social_network/lib/Local/SocialNetwork.pm
+++ Lecture8/social_network/lib/Local/SocialNetwork.pm
@@ -111,9 +111,9 @@ sub friends {
 }
 
 sub nofriends {
-  my @result = ();
+   my @result = ();
   our $test;
-  say "Who has no friends?";
+  say "Who doesn't have friends? It's them:";
 
   my $query = "SELECT * FROM users
                   WHERE NOT id IN (SELECT DISTINCT One FROM users_relation)
@@ -122,25 +122,7 @@ sub nofriends {
   $sth_rel->execute or die $dbh->errstr;
 
   my $hash_ref_nofrnd = $sth_rel->fetchall_hashref('id');
-=pod
-  my @ID_array_hasfrnd = ();
-  for my $key (keys %{$hash_ref_rel}) {
-      push @ID_array_hasfrnd, $hash_ref_rel->{$key}{'id'};
-  }
-p @ID_array_hasfrnd;
-say $#ID_array_hasfrnd+1;
 
-  $query = "SELECT * FROM users WHERE id != ? ";
-        for (1..$#ID_array_hasfrnd) {
-          $query .= " AND id != ?";
-      }
-
-  p @ID_array_hasfrnd;
-  $sth_rel = $dbh->prepare( $query ) or die $dbh->errstr;
-  $sth_rel->execute(@ID_array_hasfrnd) or die $dbh->errstr;
-  my $hash_ref_nofrnd = $sth_rel->fetchall_hashref('id');
-  #p $hash_ref_nofrnd;
-=cut
   for my $key (keys %{$hash_ref_nofrnd}) {
       push @result, $hash_ref_nofrnd->{$key};
   }
@@ -149,9 +131,81 @@ say $#ID_array_hasfrnd+1;
   return \@result;
 }
 
+sub FindHisFriends {
+    # Для num_handshakes
+    # Рекурсивная функция поиска друзей и записи в соответствующих ключ
+    # Вернёт минимальное количество рукопожатий между целевыми юзерами firstID и secondID,
+    # либо -1, если ветвь не дойдёт до $secondID
+
+    my $bundle = shift;
+    my $rootID = shift;
+    my $secondID = shift;
+    my $subtree_of_friends = shift;
+    my $who_exists_in_tree = shift;
+    my $dbh = shift;
+
+    my $We_found_him = 0; # 0 - не нашли второго друга, 1 - нашли
+    my $Last_friend = 1;
+
+    $sth_rel->execute( $rootID, $rootID ) or die $dbh->errstr;
+    my $hash_ref = $sth_rel->fetchall_hashref('Two');
+    if ($We_found_him != 0) {
+        for my $key (keys %{$hash_ref}) {
+            if ( $hash_ref->{$key}{'Two'} == $rootID ) {
+                if (est($hash_ref->{$key}{'One'}, $who_exists_in_tree)) {
+                    $subtree_of_friends->{$rootID} =
+                }
+
+            }
+            elsif ( $hash_ref->{$key}{'One'} == $rootID ) {
+
+            }
+        }
+    }
+
+}
+
 sub num_handshakes {
     say "It is num_handshakes";
-    1;
+
+    my $firstID  = shift; #Первый юзер из параметров
+    my $secondID = shift; #Второй юзер
+
+    my $tree_of_friends = {
+        'bundle_num' => 0; #Номер узла дерева, то есть номер рукопожатия
+        $firstID => {} # Дерево друзей начиная с firstID
+    };
+
+    my $who_exists_in_tree = { $firstID => '0', }; #хэш со списком всехкто уже есть в дереве, чтобы не запутывались ветви и можно было дойти до конца
+
+    #Выберем в базе данных все пары с юзером $firstID
+    my $query = "SELECT DISTINCT One, Two FROM users_realtion
+                    WHERE One = ? OR Two = ? ";
+    my $sth_rel = $dbh->prepare( $query ) or die $dbh->errstr;
+    $sth_rel->execute( $firstID, $firstID ) or die $dbh->errstr;
+    my $hash_ref = $sth_rel->fetchall_hashref('One');
+
+    # Пойдём по хэшу с полученными парами
+    for my $key (keys $hash_ref) {
+        # Выделим друга $rootID для юзера $firstID из пары $key,
+        # он будет ключом для хэша уже своих друзей
+        if ($hash_ref->{$key}{'Two'} == $firstID) {
+            my $rootID = $hash_ref->{$key}{'One'};
+        }
+        else { my $rootID = $hash_ref->{$key}{'Two'}; }
+
+        # Запишем друга в хэш со всеми юзерами, которые нам встретятся в дереве
+        $who_exists_in_tree->{$rootID} = 1;
+
+        # Теперь будем строить хэш, - искать друзей, для друга юзера
+        FindHisFriends(0, $rootID, $secondID, $tree_of_friends, $who_exists_in_tree, $dbh);
+
+    }
+
+
+
+
+    my $tree_of_friends->{$firstID};
 }
 
 sub discon { $dbh->disconnect; }
