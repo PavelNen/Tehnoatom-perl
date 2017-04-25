e34dffbba03e5ae23bf3bc2f007b852963b0050b
PavelNen <nps-11@mail.ru>
2017-04-25 20:34:03 +0300

    ВСё отлично

--- Lecture5/oop_reducer/lib/Local/Row/Simple.pm
+++ Lecture5/oop_reducer/lib/Local/Row/Simple.pm
@@ -4,6 +4,7 @@ use 5.020;
 use strict;
 use warnings;
 use DDP;
+
 use parent 'Local::Row';
 
 =encoding utf8
@@ -29,52 +30,17 @@ our $VERSION = '1.00';
 # Создаём объект класса
 sub new {
     my ($class, %param) = @_;
-
-    if ( $param{str} eq "" ) { return {}; }
-    else
-    {
-        my $now = ''; # последний попавшийся "," или ":"
-        my $key = '';
-        my $value = '';
-        my $h = {};
-        my $ln = length($param{str});
-
-        for my $i ( 0..$ln ) {
-            my $c = substr $param{str}, $i, 1;
-
-            # Проверка отсутствия значения ключа
-            if ($now eq ',' || $now eq '' and $i == $ln) {
+    my %h = ();
+    map {
+            if ($_ =~ /^(\w+):(\w+)$/) {
+                $h{$1} = $2;
+            }
+            else {
                 return undef;
-            }
-
-            # Проверка чередования "," и ":"
-            if ($c =~ /^[,:]$/
-                  and $c ne $now) {
-                $now = $c;
-            }
-            elsif ($c eq $now) {
-                return undef;
-            }
-
-            # Чтение ключа или зачения и запись их в хэш
-            if ($c =~ /^[^,:]$/) {
-                if ($now ne ':') {
-                    $key .= $c;
-                }
-                if ($now eq ':') {
-                    $value .= $c;
-                }
-            }
-            elsif ($c eq ',' || $i == $ln) {
-                $h->{$key} = $value;
-                $key = '';
-                $value = '';
             }
         }
-
-        return bless $h, $class;
-    }
-
+        (split /,/, $param{str});
+    return bless \%h, $class;
 }
 
 
