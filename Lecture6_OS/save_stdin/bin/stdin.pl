909ad51d8d9e72dfa868c4195b255fa9161d0e27
PavelNen <nps-11@mail.ru>
2017-05-06 01:51:14 +0300

    Подшлифованно

--- Lecture6_OS/save_stdin/bin/stdin.pl
+++ Lecture6_OS/save_stdin/bin/stdin.pl
@@ -4,14 +4,11 @@ use 5.020;
 use strict;
 use warnings;
 use Getopt::Long;
-use IO::Handle;
-STDOUT->autoflush(1);
-STDERR->autoflush(1);
 
 local $SIG{INT} = \&CtrlC;
-our $i = 0; # Счётчик нажатий Ctrl+C
-my $sumlth = 0; # Сумма длин строк
-my $numstr = 0; # Счётчик строк
+our $i     = 0;     # Счётчик сигналов Ctrl+C
+my $sumlth = 0;     # Сумма длин строк
+my $numstr = 0;     # Счётчик строк
 
 my $file;
 GetOptions("file=s" => \$file);
@@ -19,30 +16,25 @@ GetOptions("file=s" => \$file);
 print STDOUT "Get ready\n";
 my $fh;
 if ( open $fh, '>', "$file" ) {
-
-
-select($fh);
-while (<STDIN>) {
-    if ($i == 1) {
-        $i--;
+    select($fh);
+    while (<STDIN>) {
+        if ($i == 1) {
+            $i--;   # Если повторно не нажали Ctrl+C, то сбрасываем счётчик
+        }
+        my $s = $_;
+        chomp $s;
+        say $s;
+        $sumlth += length $s;
+        $numstr++;
     }
-    my $s = $_;
-    chomp $s;
-    say $s;
-    $sumlth += length $s;
-    $numstr++;
+
+    select(STDOUT);
+    my $avg = $sumlth / $numstr; # Средняя длина строк
+    say "$sumlth $numstr $avg";
+
+    close $fh;
 }
 
-select(STDOUT);
-my $avg = $sumlth / $numstr;
-#my $bytes = -s $fh;
-say "$sumlth $numstr $avg";
-
-close $fh;
-}
-else {
-    #print '';
-}
 sub CtrlC {
     $i++;
     if ($i == 1) {
@@ -53,8 +45,7 @@ sub CtrlC {
     if ($i == 2) {
         select(STDOUT);
         my $avg = $sumlth / $numstr;
-        #my $bytes = -s $fh;
         say "$sumlth $numstr $avg";
-        die '';
+        exit 1; # Выход без сообщений
     }
 }
