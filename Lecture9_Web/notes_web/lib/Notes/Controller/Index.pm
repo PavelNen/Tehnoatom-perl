ff01c1e0c5ce757d86c28b21ba7fe2c9bdcf16e9
PavelNen <nps-11@mail.ru>
2017-05-13 22:01:46 +0300

    init

---
+++ Lecture9_Web/notes_web/lib/Notes/Controller/Index.pm
@@ -0,0 +1,12 @@
+package Notes::Controller::Index;
+use Mojo::Base 'Mojolicious::Controller';
+
+# This action will render a template
+sub welcome {
+  my $self = shift;
+
+  # Render template "example/welcome.html.ep" with message
+  $self->render(msg => 'Добро пожаловать на сайт Заметки-онлайн!');
+}
+
+1;
