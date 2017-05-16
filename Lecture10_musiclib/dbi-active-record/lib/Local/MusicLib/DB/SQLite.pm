32cf6a739f262f2b58870818d7e4936f04923324
PavelNen <nps-11@mail.ru>
2017-05-16 17:57:31 +0300

    Local init

---
+++ Lecture10_musiclib/dbi-active-record/lib/Local/MusicLib/DB/SQLite.pm
@@ -0,0 +1,15 @@
+package Local::MusicLib::DB::SQLite;
+use Mouse;
+extends 'DBI::ActiveRecord::DB::SQLite';
+
+sub _build_connection_params {
+    my ($self) = @_;
+    return [
+        'dbi:SQLite:dbname=/tmp/muslib.db', '', '', {}
+    ];
+}
+
+no Mouse;
+__PACKAGE__->meta->make_immutable();
+
+1;
